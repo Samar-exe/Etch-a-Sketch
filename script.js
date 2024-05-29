959ba389e496d1088324f13469f3abc745c903eb
Samar-exe <samarik2004@gmail.com>
2024-05-30 03:20:05 +0530

    Remove uncessary margin styling

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
-//   div.style.margin = "20px"
   container.appendChild(div);
 }
