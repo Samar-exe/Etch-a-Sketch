7f0b179cf54a698afb721670669930bec11d08ce
Samar-exe <samarik2004@gmail.com>
2024-05-30 03:31:43 +0530

    Add Event listener for mouse
    Added a mose event listener that will call an arrow function when mouse enters a div element to create a trailing effect.

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
+  div.className = "child-div";
   container.appendChild(div);
 }
+const divs = document.querySelectorAll(".child-div");
+divs.forEach((div)=> {
+    div.addEventListener("mouseenter",()=>{
+        div.setAttribute("style","background-color:pink;");
+    });
+    div.addEventListener("mouseleave",()=>{
+        div.setAttribute("style","background-color:white;");
+    });
+});
