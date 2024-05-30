c0226f0ae7bb81f8878199346037b6be768310f0
Samar-exe <samarik2004@gmail.com>
2024-05-30 06:20:46 +0530

    Write the whole etch a skecth logic

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-const container = document.querySelector("#container");
+function redrawPad() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let newGrid = prompt("Enter new layout!");
+  if (newGrid > 100) {
+    return alert("The limit is 100");
+  }
+  for (let i = 0; i < newGrid; i++) {
+    const div = document.createElement("div");
+    div.className = "child-div";
+    container.appendChild(div);
+  }
+  trail();
+}
+//Code for Trailing Effect.
+function trail() {
+  const divs = document.querySelectorAll(".child-div");
+  divs.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = 
+        ` rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+          Math.random() * 256
+        )},${Math.floor(Math.random() * 256)})`
+      ;
+    });
+    // div.addEventListener("mouseleave", () => {
+    //   div.setAttribute("style", "background-color:white;");
+    // });
+  });
+}
 
-for (let i = 0; i < 256; i++) {
+const container = document.querySelector("#container");
+let grid = 256;
+const btn = document.querySelector("#btn");
+
+//code for redrawing Sketch Pad.
+btn.addEventListener("click", redrawPad);
+
+//Code for Creating child nodes.
+for (let i = 0; i < grid; i++) {
   const div = document.createElement("div");
   div.className = "child-div";
   container.appendChild(div);
 }
-const divs = document.querySelectorAll(".child-div");
-divs.forEach((div)=> {
-    div.addEventListener("mouseenter",()=>{
-        div.setAttribute("style","background-color:pink;");
-    });
-    div.addEventListener("mouseleave",()=>{
-        div.setAttribute("style","background-color:white;");
-    });
-});
+
+trail();
