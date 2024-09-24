19c0149b0ea62f5ccd31182e83fed9b4db23ba6c
Samar-exe <samarik2004@gmail.com>
2024-09-24 20:03:43 +0530

    Declare variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// Set default values;
+let defaultGridSize = 16;
+let penColor = "black";
+
+// Declaring some dom elements.
+let sizeChanger = document.querySelector(".size-button")
+let container = document.querySelector(".grid-container");
+
