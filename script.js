2df58283278958c32685f7f0bc37eb1957380f6e
Samar-exe <samarik2004@gmail.com>
2024-09-24 20:04:18 +0530

    Add events for changing size and drawing

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ defaultLayout(defaultGridSize);
 draw();
 
 
+// Events
+sizeChanger.addEventListener("click", askForNewGrid);
+
+function draw() {
+// Found this very cool solution here: https://stackoverflow.com/questions/30817534/how-to-implement-mousemove-while-mousedown-pressed-js
+  grids = document.querySelectorAll(".grid");
+  grids.forEach(grid => {
+    grid.addEventListener("mousemove", (e) => {
+      if (e.buttons == 1) {
+        e.target.style.backgroundColor = penColor;
+      };
+    });
+  });
+}
+
+
